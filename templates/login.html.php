caca97f1deb6c64420b3ace4b46c7c2115b5fb94
brian-w-worsham <brian.w.worsham@hotmail.com>
2019-01-13 21:24:56 -0600

    Updated the icons

--- templates/login.html.php
+++ templates/login.html.php
@@ -13,7 +13,7 @@
         <div class="col"></div>
         <div class="col">
             <form method="post" action="" class="form-signin">
-                <img class="mb-4" src="css/images/brand-logo-template.jpg" alt="BWW Apps" width="300" height="153">
+                <img class="mb-4" src="css/images/brand-logo-template.svg" alt="BWW Apps">
                 <h1 class="h3 mb-3 font-weight-normal text">Please sign in</h1>
                 <label for="email" class="sr-only">Email address</label>
                 <input type="email" id="email" name="email" class="form-control text" placeholder="Email address"
