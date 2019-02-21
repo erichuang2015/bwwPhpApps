d4e1683c5dfe74422c31a09a82bf77621c13f288
brian-w-worsham <brian.w.worsham@hotmail.com>
2019-02-21 13:49:00 -0600

    Updating rememberme implementation to use Authentication class rather than the login class to look for cookie

--- classes/BwwClasses/Controllers/Login.php
+++ classes/BwwClasses/Controllers/Login.php
@@ -27,34 +27,35 @@ class Login
         $this->storage = new FileStorage($this->storagePath);
         $this->rememberMe = new Authenticator($this->storage);
 
-        $this->loginResult = $this->rememberMe->login();
-        if ($this->loginResult->isSuccess()) {
-            $_SESSION['username'] = $this->loginResult->getCredential();
-            // There is a chance that an attacker has stolen the login token, so we store
-            // the fact that the user was logged in via RememberMe (instead of login form)
-            $_SESSION['remembered_by_cookie'] = true;
-            // print_r("remembered by cookie"); die;
-            return;
-        }
-        if ($this->loginResult->hasPossibleManipulation()) {
-            render_template("cookie_was_stolen");
-            exit();
-        }
-        // Log out when tokens have expired and user is still logged in with remember me
-        // This state can happen in two cases:
-        // a) The triples were cleared after an attack or a "global logout"
-        // b) The triples have expired
-        if ($this->loginResult->isExpired() && !empty($_SESSION['username']) && !empty($_SESSION['remembered_by_cookie'])) {
-            $this->rememberMe->clearCookie();
-            unset($_SESSION['username']);
-            unset($_SESSION['remembered_by_cookie']);
-            render_template('login', 'You were logged out because the "Remember Me" cookie was no longer valid.');
-            exit;
-        }
-        if ($this->loginResult->isExpired() && !empty($_SESSION['username'])) {
-            // Do rate limiting here. Lots of requests for non-existing triplets can be an indicator of a brute force attack
-            sleep(5);
-        }
+        // $this->loginResult = $this->rememberMe->login();
+        // if ($this->loginResult->isSuccess()) {
+        //     $_SESSION['username'] = $this->loginResult->getCredential();
+        //     // print_r($_SESSION['username']); die;
+        //     // There is a chance that an attacker has stolen the login token, so we store
+        //     // the fact that the user was logged in via RememberMe (instead of login form)
+        //     $_SESSION['remembered_by_cookie'] = true;
+        //     // print_r("remembered by cookie"); die;
+        //     return;
+        // }
+        // if ($this->loginResult->hasPossibleManipulation()) {
+        //     // render_template("cookie_was_stolen");
+        //     exit();
+        // }
+        // // Log out when tokens have expired and user is still logged in with remember me
+        // // This state can happen in two cases:
+        // // a) The triples were cleared after an attack or a "global logout"
+        // // b) The triples have expired
+        // if ($this->loginResult->isExpired() && !empty($_SESSION['username']) && !empty($_SESSION['remembered_by_cookie'])) {
+        //     $this->rememberMe->clearCookie();
+        //     unset($_SESSION['username']);
+        //     unset($_SESSION['remembered_by_cookie']);
+        //     // render_template('login', 'You were logged out because the "Remember Me" cookie was no longer valid.');
+        //     exit;
+        // }
+        // if ($this->loginResult->isExpired() && !empty($_SESSION['username'])) {
+        //     // Do rate limiting here. Lots of requests for non-existing triplets can be an indicator of a brute force attack
+        //     sleep(5);
+        // }
     }
 
     public function loginForm()
--- classes/utilityClasses/Authentication.php
+++ classes/utilityClasses/Authentication.php
@@ -1,20 +1,39 @@
 <?php
 namespace UtilityClasses;
 
+use Birke\Rememberme\Authenticator;
+use Birke\Rememberme\Storage\FileStorage;
+
 class Authentication
 {
     private $users; // $users is the users table
     private $usernameColumn;
     private $passwordColumn;
-    // need recovery question vars
+
+    private $storagePath;
+    private $storage;
+    private $rememberMe;
+    private $loginResult;
 
     public function __construct(DatabaseTable $users, $usernameColumn, $passwordColumn)
-    { // need recovery question params
+    {
         session_start();
         $this->users = $users;
         $this->usernameColumn = $usernameColumn;
         $this->passwordColumn = $passwordColumn;
-        // need recovery question vars
+
+        // Initialize RememberMe Library with file storage
+        $this->storagePath = dirname(__FILE__) . "/../../tokens/";
+        // $newDirName = __DIR__ . '/../../../public/uploads/' . $newName;
+        if (!is_writable($this->storagePath) || !is_dir($this->storagePath)) {
+            die(
+                "'$this->storagePath' does not exist or is not writable by the web server.\n" .
+                "To run the example, please create the directory and give it the correct permissions."
+            );
+        }
+        $this->storage = new FileStorage($this->storagePath);
+        $this->rememberMe = new Authenticator($this->storage);
+
     }
 
     public function login($username, $password)
@@ -33,6 +52,37 @@ class Authentication
 
     public function isLoggedIn()
     {
+        $this->loginResult = $this->rememberMe->login();
+        if ($this->loginResult->isSuccess()) {
+            $_SESSION['username'] = $this->loginResult->getCredential();
+            // print_r($_SESSION['username']); die;
+            // There is a chance that an attacker has stolen the login token, so we store
+            // the fact that the user was logged in via RememberMe (instead of login form)
+            $_SESSION['remembered_by_cookie'] = true;
+            // print_r("remembered by cookie"); die;
+            return true;
+        }
+        if ($this->loginResult->hasPossibleManipulation()) {
+            // render_template("cookie_was_stolen");
+            exit();
+        }
+        // Log out when tokens have expired and user is still logged in with remember me
+        // This state can happen in two cases:
+        // a) The triples were cleared after an attack or a "global logout"
+        // b) The triples have expired
+        if ($this->loginResult->isExpired() && !empty($_SESSION['username']) && !empty($_SESSION['remembered_by_cookie'])) {
+            $this->rememberMe->clearCookie();
+            unset($_SESSION['username']);
+            unset($_SESSION['remembered_by_cookie']);
+            // render_template('login', 'You were logged out because the "Remember Me" cookie was no longer valid.');
+            exit;
+        }
+        if ($this->loginResult->isExpired() && !empty($_SESSION['username'])) {
+            // Do rate limiting here. Lots of requests for non-existing triplets can be an indicator of a brute force attack
+            sleep(5);
+        }
+
+        //old session management below:
         if (empty($_SESSION['username'])) {
             return false;
         }
@@ -63,15 +113,15 @@ class Authentication
         $secondAnswer = strtolower($secondAnswer);
         $thirdAnswer = strtolower($thirdAnswer);
 
-		$user = $this->users->find($this->usernameColumn, $email);
-		// print_r($user[0]['id']); die;
+        $user = $this->users->find($this->usernameColumn, $email);
+        // print_r($user[0]['id']); die;
 
         //Need to replace this if block with checking of the security answers
         if (!empty($user) && password_verify($firstAnswer, $user[0]['firstanswer']) && password_verify($secondAnswer, $user[0]['secondanswer']) && password_verify($thirdAnswer, $user[0]['thirdanswer'])) {
             // Generate a new temp password
             $tempPassword = $this->random_password();
             $tempPasswordHashed = password_hash($tempPassword, PASSWORD_DEFAULT);
-			// Update the db with the new $tempPassword
+            // Update the db with the new $tempPassword
 
             $accountInfo = $this->users->find('id', $user[0]['id']);
             $accountData['id'] = (int) $user[0]['id'];
