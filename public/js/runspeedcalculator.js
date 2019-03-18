23a21fc04d090c35f423d4d9926a9630e439d756
brian-w-worsham <brian.w.worsham@hotmail.com>
2019-03-18 14:47:56 -0500

    Run Speed Calculator: Updates to logic

--- public/js/runspeedcalculator.js
+++ public/js/runspeedcalculator.js
@@ -1,37 +1,57 @@
 /*jshint esversion: 6 */
 
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+            }, false);
+        });
+    }, false);
+})();
+
+$(document).ready(function () {
+    "use strict";
+    $("#btnSubmitRunSpeed").on("keyup click", function(e){
+        if (e.keycode == 13 || e.which == 13 || e.keycode == 32 || e.which == 32 || e.type == "click") {
+            e.preventDefault();
+            e.stopPropagation();
+            getMilesPerHour();
+        }
+    });
+});
+
 function getMilesPerHour() {
     "use strict";
-    var distanceVal = $("#dist-output").val();
-    var whiteSpace = distanceVal.indexOf(" ");
-    var distNum = distanceVal.slice(0, whiteSpace);
-    //distNum = distNum.trim();
-    var minutesVal = $("#time-output").val();
-    var minuteWhiteSpace = minutesVal.indexOf(" ");
-    var minutesNum = minutesVal.slice(0, minuteWhiteSpace);
-    var mph = runSpeedDistCalculator(distNum, minutesNum);
-    $('#mph-paragraph').removeAttr('hidden').show();
-    $("#mph-paragraph").text("You must run " + mph + " mph to acheive your goal.");
+    var distanceVal = $("#distance").val();
+    var minutesVal = $("#runMinutes").val();
+    var secondsVal = $("#runSeconds").val();
+    var mph = runSpeedDistCalculator(distanceVal, minutesVal, secondsVal);
+    $('#mphParagraph').removeAttr('hidden').show();
+    $("#mphParagraph").text("You must run " + mph + " mph to acheive your goal.");
 }
 
 
-function runSpeedDistCalculator(dist, time)
+function runSpeedDistCalculator(dist, m, s)
 {
     "use strict";
-    let miles = dist;
-    let minutes = time;
+    dist = parseFloat(dist);
+    m = parseFloat(m);
+    s = parseFloat(s);
+    let secondsPerMinute = 60.0;
+    let minuteFraction = s / secondsPerMinute;
+    let miles = dist; // 6
+    let minutes = m + minuteFraction;
     let hour = 60;
-    let mpm = minutes / miles; // minutes per mile
-    let miesPh = hour / mpm;
-    miesPh = miesPh.toFixed(2);
-    return miesPh;
+    let mpm = minutes / miles;
+    let milesPerHour = hour / mpm;
+    milesPerHour = milesPerHour.toFixed(2);
+    return milesPerHour;
 }
-
-function distUpdate(miles)
-{
-    document.querySelector('#dist-output').value = miles;
-    (miles > 1) ? $('#dist-output').append(" miles") : $('#dist-output').append(" mile");
-}
-
-
-
--- templates/runspeedcalculator.html.php
+++ templates/runspeedcalculator.html.php
@@ -1,4 +1,4 @@
-<!-- <link rel="stylesheet" href="/css/gladiator.css"> -->
+<link rel="stylesheet" href="/css/runspeed.css">
 <div class="jumbotron">
     <div class="container">
         <h1 class="display-3">Run Speed Calculator</h1>
@@ -7,49 +7,30 @@
 <div class="container fill-height">
     <h2>This program will help you determine how fast you need to run in order to reach a specified distance
         within an alloted amount of time.</h2>
-    <div id="runSpeed-input-form">
+    <form action="" method="" id="runSpeed-input-form" class="needs-validation">
         <div id="dist-time-input">
-            <br>
             <ol class="run-speed-ol">
                 <li>
                     <p class="run-speed">How far do you want to run?</p>
-                    <label for=dist-range>Distance:</label>
-                    <input type=range min=1 max=24 value=3 id=dist-range step=0.01 list=distance-settings oninput="distUpdate(value)">
-                    <datalist id=distance-settings>
-                        <option>1</option>
-                        <option>6</option>
-                        <option>12</option>
-                        <option>18</option>
-                        <option>24</option>
-                    </datalist>
-                    <output for=dist-range id=dist-output>3 miles</output>
+                    <label for="distance">Distance:</label>
+                    <input type="number" min="1" max="24" value="3" id="distance" step="0.01" class="form-control" required autofocus>
+                    <span>miles</span>
+                    <div class="invalid-feedback"><span id="distanceError"></span></div>
                 </li>
                 <li>
-                    <p class="run-speed">How much time do you have available?</p>
-                    <label for=run-time>Time:</label>
-                    <input type=range min=10 max=120 value=60 id=run-time step=1 list=time-settings oninput="timeUpdate(value)">
-                    <datalist id=time-settings>
-                        <option>10</option>
-                        <option>30</option>
-                        <option>60</option>
-                        <option>90</option>
-                        <option>120</option>
-                    </datalist>
-                    <output for=run-time id=time-output>60 minutes</output>
-                    <script>
-                        function timeUpdate(mins) {
-                            document.querySelector('#time-output').value = mins;
-                            $('#time-output').append(" minutes");
-                        }
-                    </script>
+                    <p class="run-speed">How much time will you spend running?</p>
+                    <label for="runMinutes">Minutes:</label>
+                    <input type="number" min="1" max="300" value="60" id="runMinutes" step="1" class="form-control" required>
+                    <div class="invalid-feedback"><span id="runMinutesError"></span></div>
+
+                    <label for="runSeconds">Seconds:</label>
+                    <input type="number" min="0" max="59" value="00" id="runSeconds" step="1" class="form-control" required>
+                    <div class="invalid-feedback"><span id="runSecondsError"></span></div>
                 </li>
             </ol>
-            <br>
-            <p hidden="" id="mph-paragraph" class="required-mph-paragraph"></p><br>
-            <input type="submit" value="Submit" id="btn-submit-runSpeed" name="btn-submit-runSpeed" class="btn btn-primary"
-                onclick="getMilesPerHour()">
-            <br>
+            <p hidden id="mphParagraph"></p>
+            <button id="btnSubmitRunSpeed" name="btnsubmitrunspeed" class="btn btn-primary">Submit</button>
         </div>
-    </div>
+    </form>
 </div>
 <script type="text/javascript" src="/js/runspeedcalculator.js"></script>
