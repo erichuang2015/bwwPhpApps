82dd55a5d868508a3119e2159a21cdd602dcd166
brian-w-worsham <brian.w.worsham@hotmail.com>
2019-07-04 10:20:38 -0500

    Reverting to the state at cb62a4f3c59e937a17d73e60041cfc3c9c1c9978

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,8 +1,9 @@
 <?php
 $pdo = NULL;
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=homestead;charset=utf8', 'homestead', 'secret');
-} catch (\PDOException $e) {
+    $pdo = new PDO('mysql:host=localhost;dbname=bwwlocaldb;charset=utf8', 'bworsham', '153756Pw');
+}
+catch (\PDOException $e) {
     $pdo = new PDO('mysql:host=localhost;dbname=bwwdb;charset=utf8', 'bworsham', '153756Pw');
 }
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
