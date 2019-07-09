4de8e336717891092b5a635583485ba26d403f26
brian-w-worsham <brian.w.worsham@hotmail.com>
2019-07-08 21:06:58 -0500

    About: updated about page

--- templates/about.html.php
+++ templates/about.html.php
@@ -12,21 +12,25 @@
             <p class="card-text">The below list of technologies were used to develop this website:</p>
             <dl>
                 <dt><a href="https://www.vagrantup.com/" target="_blank">Vagrant</a></dt>
-                <dd>Used to provision and run a virtual private Linux Unbutu server</dd>
+                <dd>- Used to provision and run a virtual private Linux Unbutu server</dd>
                 <dt> <a href="https://github.com/Swader/homestead_improved" target="_blank">Homestead Improved</a></dt>
-                <dd>Used to provide a pre-packaged Vagrant box with all server already configured</dd>
+                <dd>- Used to provide a pre-packaged Vagrant box with all server already configured</dd>
                 <dt> <a href="https://www.virtualbox.org/" target="_blank">Virtual Box</a></dt>
-                <dd>Used to create a virtual machine where the server resides</dd>
+                <dd>- Used to create a virtual machine where the server resides</dd>
                 <dt> <a href="https://www.php.net/" target="_blank">PHP7.3</a></dt>
-                <dd>Used to write the server code and interact with the database</dd>
+                <dd>- Used to write the server code and interact with the database</dd>
                 <dt> <a href="https://www.ecma-international.org/ecma-262/6.0/" target="_blank">JavaScript ECMA6</a></dt>
-                <dd>Used to write all the front end code</dd>
+                <dd>- Used to write all the front end code</dd>
                 <dt> <a href="https://www.w3.org/TR/CSS/#css" target="_blank">CSS3</a></dt>
-                <dd>Used to apply custum styling the pages</dd>
+                <dd>- Used to apply custum styling the pages</dd>
                 <dt> <a href="https://www.w3.org/TR/2017/REC-html52-20171214/" target="_blank">HTML5</a></dt>
-                <dd>Used to structure the web pages</dd>
+                <dd>- Used to structure the web pages</dd>
                 <dt> <a href="https://getbootstrap.com/" target="_blank">Bootstrap 4</a></dt>
-                <dd>This is a rich styling library that greatly simplified the process styling this website</dd>
+                <dd>- This is a rich styling library that greatly simplified the process styling this website</dd>
+                <dt> <a href="https://git-scm.com/" target="_blank">Git</a></dt>
+                <dd>- Used for version control</dd>
+                <dt> <a href="https://github.com/PHPMailer/PHPMailer" target="_blank">PHPMailer</a></dt>
+                <dd>- Used to create and send emails from the server</dd>
             </dl>
         </div>
     </div>
