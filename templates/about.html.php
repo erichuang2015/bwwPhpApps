3f8bf3b56ef5797f157efa061c392d997dce80d7
brian-w-worsham <brian.w.worsham@hotmail.com>
2019-07-12 22:57:04 -0500

    About, Sigin, and TodoList style updates

--- templates/about.html.php
+++ templates/about.html.php
@@ -7,57 +7,63 @@
 <div class="container fill-height">
     <div class="card mb-5">
         <img src="/css/images/markus-spiske-xekxE_VR0Ec-unsplash.jpg" class="img-fluid" alt="A picture of the site developer, Brian Worsham">
+        <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+           href="https://unsplash.com/@markusspiske?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Markus Spiske"><span style="display:inline-block;padding:2px 3px"><svg
+                        xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Markus Spiske</span></a>
         <div class="card-body">
-            <h5 class="card-title">Technologies Used to Develop this Site:</h5>
+            <h5 class="card-title">Technologies Used to Develop this Site</h5>
             <p class="card-text">The below list of technologies were used to develop this website:</p>
             <dl>
                 <dt><a href="https://www.vagrantup.com/" target="_blank">Vagrant</a></dt>
                 <dd>- Used to provision and run a virtual private Linux Unbutu server</dd>
-                <dt> <a href="https://github.com/Swader/homestead_improved" target="_blank">Homestead Improved</a></dt>
+                <dt><a href="https://github.com/Swader/homestead_improved" target="_blank">Homestead Improved</a></dt>
                 <dd>- Used to provide a pre-packaged Vagrant box with the server already configured</dd>
-                <dt> <a href="https://www.virtualbox.org/" target="_blank">Virtual Box</a></dt>
+                <dt><a href="https://www.virtualbox.org/" target="_blank">Virtual Box</a></dt>
                 <dd>- Used to create a virtual machine where the server resides</dd>
-                <dt> <a href="https://www.php.net/" target="_blank">PHP7.3</a></dt>
+                <dt><a href="https://www.php.net/" target="_blank">PHP7.3</a></dt>
                 <dd>- Used to write the server code and interact with the database</dd>
-                <dt> <a href="https://www.mysql.com/" target="_blank">MySQL</a></dt>
+                <dt><a href="https://www.mysql.com/" target="_blank">MySQL</a></dt>
                 <dd>- This is the SQL language used for the database</dd>
-                <dt> <a href="https://www.ecma-international.org/ecma-262/6.0/" target="_blank">JavaScript ECMA6</a></dt>
+                <dt><a href="https://www.ecma-international.org/ecma-262/6.0/" target="_blank">JavaScript ECMA6</a></dt>
                 <dd>- Used to write all the front end code</dd>
-                <dt> <a href="https://jquery.com/" target="_blank">jQuery</a></dt>
+                <dt><a href="https://jquery.com/" target="_blank">jQuery</a></dt>
                 <dd>- This is a powerful javascript library which simplifies selecting elements within the document object model.</dd>
-                <dt> <a href="https://gijgo.com/datepicker" target="_blank">GIJGO</a></dt>
+                <dt><a href="https://gijgo.com/datepicker" target="_blank">GIJGO</a></dt>
                 <dd>- Used their Datepicker plug-in for the front end calendar functionality.</dd>
-                <dt> <a href="https://www.w3.org/TR/CSS/#css" target="_blank">CSS3</a></dt>
+                <dt><a href="https://www.w3.org/TR/CSS/#css" target="_blank">CSS3</a></dt>
                 <dd>- Used to apply custom styling to the pages</dd>
-                <dt> <a href="https://www.w3.org/TR/2017/REC-html52-20171214/" target="_blank">HTML5</a></dt>
+                <dt><a href="https://www.w3.org/TR/2017/REC-html52-20171214/" target="_blank">HTML5</a></dt>
                 <dd>- Used to structure the web pages</dd>
-                <dt> <a href="https://getbootstrap.com/" target="_blank">Bootstrap 4</a></dt>
+                <dt><a href="https://getbootstrap.com/" target="_blank">Bootstrap 4</a></dt>
                 <dd>- This is a rich styling library that greatly simplified the process styling this website</dd>
-                <dt> <a href="https://git-scm.com/" target="_blank">Git</a></dt>
+                <dt><a href="https://git-scm.com/" target="_blank">Git</a></dt>
                 <dd>- Used for version control</dd>
-                <dt> <a href="https://github.com/PHPMailer/PHPMailer" target="_blank">PHPMailer</a></dt>
+                <dt><a href="https://github.com/PHPMailer/PHPMailer" target="_blank">PHPMailer</a></dt>
                 <dd>- Used to create and send emails from the server</dd>
-                <dt> <a href="https://fontawesome.com/" target="_blank">Font Awesome</a></dt>
+                <dt><a href="https://fontawesome.com/" target="_blank">Font Awesome</a></dt>
                 <dd><span>- Font awesome is where I got the </span><i class="fas fa-calendar"></i><span class="sr-only">calendar</span> <span>icons for the To Do List page</span></dd>
-                <dt> <a href="https://fontawesome.com/" target="_blank">Open Iconic</a></dt>
+                <dt><a href="https://fontawesome.com/" target="_blank">Open Iconic</a></dt>
                 <dd><span>- Open Iconic is where I got the </span><img src="/css/vendor/open-iconic-master/svg/info.svg" alt="Information icon" width="12px" height="12px"><span class="sr-only">information</span> <span> tooltip icons used throughout the site</span></dd>
             </dl>
             <p class="lead font-weight-bold"><span>If you would like to view the source code for this website you may do so at: </span><a href="https://github.com/brian-w-worsham/bwwPhpApps" target="_blank">My Github Repo</a></p>
         </div>
     </div>
     <div class="card">
-        <img src="/css/images/markus-spiske-xekxE_VR0Ec-unsplash.jpg" class="img-fluid" alt="A picture of the site developer, Brian Worsham">
+        <img src="/css/images/ross-findon-mG28olYFgHI-unsplash.jpg" class="img-fluid" alt="Photo by Ross Findon on Unsplash">
+        <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+           href="https://unsplash.com/@rossf?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Ross Findon"><span style="display:inline-block;padding:2px 3px"><svg
+                        xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Ross Findon</span></a>
         <div class="card-body">
-            <h5 class="card-title">Future Updates:</h5>
+            <h5 class="card-title">Future Updates</h5>
             <p class="card-text">The following list details improvements coming to this site in the near future.</p>
             <dl>
-                <dt>Spanish Translations:</dt>
+                <dt>Spanish Translations</dt>
                 <dd>- There will be a button users can toggle to view the site in Spanish or English</dd>
-                <dt>Help Page:</dt>
+                <dt>Help Page</dt>
                 <dd>- This will be a page that allows the user to report defects or issues they are experiencing with the site</dd>
-                <dt>Section 508 Accessibility:</dt>
+                <dt>Section 508 Accessibility</dt>
                 <dd>- Accessiblity concerns will be addressed across every page of the site</dd>
-                <dt>Search Engine Optimization (SEO):</dt>
+                <dt>Search Engine Optimization (SEO)</dt>
                 <dd>- Bring this site to the public</dd>
             </dl>
         </div>
@@ -65,7 +71,7 @@
     <div class="card">
         <img src="/css/images/bworsham_profile.png" class="img-fluid" alt="A picture of the site developer, Brian Worsham">
         <div class="card-body">
-            <h5 class="card-title">Site developer:</h5><span class="font-weight-bold">Brian Worsham</span>
+            <h5 class="card-title">Site developer</h5><span class="font-weight-bold">Brian Worsham</span>
             <h6 class="font-italic">Software Developer and Army Veteran in Central Texas</h6>
             <p class="card-text">Software Developer by day, Fitness Fanatic by night. Brian is a hard working Texan who
                 enjoys developing high quality software programs.</p>
